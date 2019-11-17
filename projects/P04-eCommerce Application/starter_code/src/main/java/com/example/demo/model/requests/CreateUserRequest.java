3c1d37af802c5689d01ecce081ca8b50e4921e4d
Krist14n <kristian.tapia01@gmail.com>
2019-11-17 14:51:21 -0800

    add splunk event logging

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -2,10 +2,21 @@ package com.example.demo.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotEmpty;
+
 public class CreateUserRequest {
 
 	@JsonProperty
+	@NotEmpty
 	private String username;
+
+	@JsonProperty
+	@NotEmpty
+	private String password;
+
+	@JsonProperty
+	@NotEmpty
+	private String confirmPassword;
 
 	public String getUsername() {
 		return username;
@@ -13,5 +24,21 @@ public class CreateUserRequest {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
 }
